d69b208edfff414e1c84c588018eb8647e7dbcfd
Emily Wise <wise.m.emily@gmail.com>
2017-12-13 10:29:05 -0700

    add working api call with all parameters included- no refactor

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
 class SearchController < ApplicationController
 
   def index
-  end  
+    response = Faraday.get 'https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest?format=json&api_key=nNyQewlqUIYl6CmQdPRTDzcjhYkl9AaUR6Ar5rZ3&location=80203&radius=6.0&fuel_type'
+    get_json= JSON.parse(response.body, symbolize_names: true)
+
+  byebug
+  end
 
 end
