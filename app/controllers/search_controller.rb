64897cad20776f4ebac48b075c19ffb82381732d
Emily Wise <wise.m.emily@gmail.com>
2017-12-13 10:42:37 -0700

    add station model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,18 @@ class SearchController < ApplicationController
 
   def index
     response = Faraday.get 'https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest?format=json&api_key=nNyQewlqUIYl6CmQdPRTDzcjhYkl9AaUR6Ar5rZ3&location=80203&radius=6.0&fuel_type'
-    get_json= JSON.parse(response.body, symbolize_names: true)
+    get_json= JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
 
-  byebug
+    get_json.map do |raw_data|
+      Station.new(raw_data)
+      byebug
+
+
+    end
+
+
+
+
   end
 
 end
---
+++ app/models/station.rb
@@ -0,0 +1,13 @@
+class Station
+
+  attr_reader :name, :address, :fuel_type, :distance, :access_time
+
+  def initialize(attrs = {})
+    @name = attrs[:station_name]
+    @address = attrs[:street_address]
+    @fuel_type = attrs[:fuel_type_code]
+    @distance = attrs[:distance]
+    @access_time = attrs[:access_days_time]
+  end
+
+end
